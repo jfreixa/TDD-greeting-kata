69829878aed3be5975558211693bf86e7c98bb74
Jordi Freixa <jordi@howaboutsales.com>
2018-02-02 16:15:41 +0100

    Refactoring tests

--- greeting.test.js
+++ greeting.test.js
@@ -1,37 +1,40 @@
 import greeting from "./greeting";
 
+const testGreeting = (input, output) => {
+  expect(greeting(input)).toBe(output);
+};
+
 test("interpolates name in a simple greeting", () => {
-  expect(greeting("Bob")).toBe("Hello, Bob.");
-  expect(greeting("Allie")).toBe("Hello, Allie.");
+  testGreeting("Bob", "Hello, Bob.");
+  testGreeting("Allie", "Hello, Allie.");
 });
 test("Handle nulls by introducing a stand-in", () => {
-  const name = null;
-  expect(greeting(name)).toBe("Hello, my friend.");
+  testGreeting(null, "Hello, my friend.");
 });
 test("Handle shouting.", () => {
-  expect(greeting("JERRY")).toBe("HELLO JERRY!");
-  expect(greeting("DAN")).toBe("HELLO DAN!");
+  testGreeting("JERRY", "HELLO JERRY!");
+  testGreeting("DAN", "HELLO DAN!");
 });
 test("Handle two names of input.", () => {
   const names = ["Jill", "Jane"];
-  expect(greeting(names)).toBe("Hello, Jill and Jane.");
+  testGreeting(names, "Hello, Jill and Jane.");
 });
 
 test("Handle arbitrarily names of input.", () => {
   const names = ["Amy", "Brian", "Charlotte"];
-  expect(greeting(names)).toBe("Hello, Amy, Brian, and Charlotte.");
+  testGreeting(names, "Hello, Amy, Brian, and Charlotte.");
 });
 
 test("Allow mixing of normal and shouted names by separating the response into two greetings.", () => {
   const names = ["Amy", "BRIAN", "Charlotte"];
-  expect(greeting(names)).toBe("Hello, Amy and Charlotte. AND HELLO BRIAN!");
+  testGreeting(names, "Hello, Amy and Charlotte. AND HELLO BRIAN!");
 });
 
 test("If any entries in name are a string containing a comma, split it as its own input.", () => {
   const names = ["Bob", "Charlie, Dianne"];
-  expect(greeting(names)).toBe("Hello, Bob, Charlie, and Dianne.");
+  testGreeting(names, "Hello, Bob, Charlie, and Dianne.");
 });
 test("Allow the input to escape intentional commas introduced by Requirement 7", () => {
   const names = ["Bob", '"Charlie, Dianne"'];
-  expect(greeting(names)).toBe("Hello, Bob and Charlie, Dianne.");
+  testGreeting(names, "Hello, Bob and Charlie, Dianne.");
 });
